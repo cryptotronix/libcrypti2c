fd6b2845786c28cc8912541eb895bad41a5b1b0b
Josh Datko <jbd@cryptotronix.com>
2014-05-24 16:19:30 +0000

    Comments out guile extension as it appears not be compatible with Guile 1.8

--- crypti2c/guile_ext.c
+++ crypti2c/guile_ext.c
@@ -22,33 +22,27 @@
 
 #warning GUILE EXTENSIONS NOT YET TESTED
 
-SCM send_recv_wrapper (SCM scm_fd, SCM scm_data_to_send, SCM scm_recv_len,
-                       SCM wait_time)
-{
+/* SCM send_recv_wrapper (SCM scm_fd, SCM scm_data_to_send, SCM scm_recv_len, */
+/*                        SCM wait_time) */
+/* { */
 
-    int fd = scm_to_int (scm_fd);
-    uint8_t * buf = SCM_BYTEVECTOR_CONTENTS (scm_data_to_send);
-    size_t len = SCM_BYTEVECTOR_LENGTH (scm_data_to_send);
-    int recv_len = scm_to_int (scm_recv_len);
-    SCM scm_rsp = scm_c_make_bytevector (recv_len);
-    struct timespec t = {};
+/*     int fd = scm_to_int (scm_fd); */
+/*     uint8_t * buf = SCM_BYTEVECTOR_CONTENTS (scm_data_to_send); */
+/*     size_t len = SCM_BYTEVECTOR_LENGTH (scm_data_to_send); */
+/*     int recv_len = scm_to_int (scm_recv_len); */
+/*     SCM scm_rsp = scm_c_make_bytevector (recv_len); */
+/*     struct timespec t = {}; */
 
-    enum CI2C_STATUS_RESPONSE rsp = RSP_COMM_ERROR;
+/*     enum CI2C_STATUS_RESPONSE rsp = RSP_COMM_ERROR; */
 
-    rsp = ci2c_send_and_receive (fd, buf, len,
-                                 SCM_BYTEVECTOR_CONTENTS (scm_rsp),
-                                 recv_len,
-                                 &t);
+/*     rsp = ci2c_send_and_receive (fd, buf, len, */
+/*                                  SCM_BYTEVECTOR_CONTENTS (scm_rsp), */
+/*                                  recv_len, */
+/*                                  &t); */
 
-    return scm_cons (scm_from_int (rsp), scm_rsp);
+/*     return scm_cons (scm_from_int (rsp), scm_rsp); */
 
-}
-
-SCM
-j0_wrapper (SCM x)
-{
-    return scm_from_double (j0 (scm_to_double (x)));
-}
+/* } */
 
 void
 acquire_bus_wrapper (SCM fd, SCM addr)
