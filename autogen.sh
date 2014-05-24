7666f129c743c36cf8014302f78ae0dc631c87d1
Josh Datko <jbd@cryptotronix.com>
2014-05-24 02:34:37 +0000

    Fixes autogen to create m4 directory.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,24 @@
 #!/bin/sh -e
-test -n "$srcdir" || srcdir=`dirname "$0"`
-test -n "$srcdir" || srcdir=.
+# Copyright (C) 2014 Cryptotronix, LLC.
 
-autoreconf --force --install --verbose "$srcdir"
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+# This file is part of libcrypti2c.
+
+# libcrypti2c is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# libcrypti2c is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with libcrypti2c.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ ! -d "m4" ]; then
+    mkdir m4
+fi
+autoreconf --force --install
+./configure
+make
