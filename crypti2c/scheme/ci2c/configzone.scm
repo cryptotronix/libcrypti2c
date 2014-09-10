6060ebca0163a007476d52d2bf3bb9012f01477d
Josh Datko <jbd@cryptotronix.com>
2014-09-10 15:44:03 -0600

    Adds code to serialize commands and apply crcs

--- crypti2c/scheme/ci2c/configzone.scm
+++ crypti2c/scheme/ci2c/configzone.scm
@@ -60,3 +60,57 @@ contiguous bytes of the entire configuration zone"
   (string-join (map (lambda (e) (format #f "~2,'0x" e))
                     (bytevector->u8-list bv))
                ":"))
+
+;; commands
+(define-immutable-record-type <atsha204-command>
+  (make-atsha204-command opcode param1 param2 data)
+  atsha204-command?
+  (opcode atsha204-command-opcode)
+  (param1 atsha204-command-param1)
+  (param2 atsha204-command-param2)
+  (data atsha204-command-data))
+
+(define (make-zone-bits zone-sym)
+  (case zone-sym
+    ((CONFIG-ZONE) #x00)
+    ((OTP-ZONE) #x01)
+    ((DATA-ZONE) #x02)))
+
+(define (make-write4-cmd zone addr data)
+  (make-atsha204-command 'COMMAND-WRITE (make-zone-bits zone) addr data))
+
+(define opcodes (make-hash-table))
+
+(hash-set! opcodes 'COMMAND-WRITE #x12)
+
+(define (atsha204-command-apply-crc bv)
+  "Apply the CRC to the already serialized command. Requires some byte moving"
+  (let* ([crc (ci2c-crc16 (u8-list->bytevector (cdr (bytevector->u8-list bv))))]
+         [bv-len (bytevector-length bv)]
+         [crc-len (bytevector-length crc)]
+         [bv-crc-len (+ bv-len crc-len)]
+         [bv-crc (make-bytevector bv-crc-len)])
+    (begin (bytevector-copy! bv 0 bv-crc 0 bv-len)
+           (bytevector-copy! crc 0 bv-crc (- bv-crc-len crc-len) crc-len)
+           bv-crc)))
+
+(define (atsha204-command-serialize cmd)
+  "Serialize the command for transmission to the device.
+This applies the CRC as well."
+  (let* ([len-sans-crc (+ 1 1 1 1 2 (length (atsha204-command-data cmd)))]
+         [bv (make-bytevector len-sans-crc)]
+         [packet-size (+ (- len-sans-crc 1) 2)]
+         [COMMAND-BYTE-INDICATOR #x03])
+    (begin (bytevector-u8-set! bv 0 COMMAND-BYTE-INDICATOR)
+           (bytevector-u8-set! bv 1 packet-size)
+           (bytevector-u8-set! bv 2
+                               (hash-ref opcodes (atsha204-command-opcode cmd)))
+           (bytevector-u8-set! bv 3
+                               (atsha204-command-param1 cmd))
+           (bytevector-u16-set! bv 4
+                                (atsha204-command-param2 cmd) (endianness little))
+           (if (not (null? (atsha204-command-data cmd)))
+               (let ([data (u8-list->bytevector (atsha204-command-data cmd))])
+                 (bytevector-copy! data 0 bv 6
+                                   (length (atsha204-command-data cmd)))))
+           (atsha204-command-apply-crc bv))))
