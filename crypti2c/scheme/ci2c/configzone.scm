59cb5eea8e52564a6d51db3fa4637b4319c830d7
Josh Datko <jbd@cryptotronix.com>
2014-09-09 20:09:15 -0600

    Adds documentation.
    Adds doc strings to functions.

--- crypti2c/scheme/ci2c/configzone.scm
+++ crypti2c/scheme/ci2c/configzone.scm
@@ -11,38 +11,47 @@
 (load-extension "/usr/local/lib/libcrypti2c-0.1" "init_crypti2c")
 
 (define (is-ecc-content? x)
+  "Predicate to test if the symbol X matches the start symbol
+for the ECC108 Content block"
   (if (equal? 'ECC108Content.01 x)
       #t
       #f))
 
 (define (get-ecc108-content sxmldata)
+  "Returns the ECC108 list from the entire SXMLDATA list"
   (let ([top-list (cdr sxmldata)])
     (car (filter (lambda [x] (is-ecc-content? (car x))) top-list))))
 
 (define (get-config-data ecc108-content)
+  "Returns the inner list of the actual data"
   (caddr ecc108-content))
 
 (define (get-config-elements config-data)
+  "Strips out extraneous information in the list and leaves just lists of
+elements"
   (filter list? config-data))
 
-(define hexstring->intlist
-  (lambda [hexstring]
-    (map (lambda (x) (string->number x 16)) (string-tokenize hexstring))))
+(define (hexstring->intlist hexstring)
+  "Converts the hexstring in XML data to an int list"
+  (map (lambda (x) (string->number x 16)) (string-tokenize hexstring)))
 
-(define elementstr->elementint
-  (lambda [element]
-    (cons (car element) (list (hexstring->intlist (cadr element)) ))))
 
-(define xml-file-port->config-bv
-  (lambda [port]
-    (let* ([top (xml->sxml port)]
-           [config (get-config-elements (get-config-data (get-ecc108-content top)))]
-           [byte-list (map (lambda [x] (elementstr->elementint x)) config)])
-      (u8-list->bytevector (concatenate (map cadr byte-list))))))
+(define (elementstr->elementint element)
+  "Converts the elements, which are strings of hex into an integer list"
+  (cons (car element) (list (hexstring->intlist (cadr element)) )))
 
-(define xml-file->config-bv
-  (lambda [filename]
-    (call-with-input-file filename xml-file-port->config-bv)))
+(define (xml-file-port->config-bv port)
+  "Take the open XML file PORT, parse it, extract the configuration zone,
+and return the byte vector of the zone"
+  (let* ([top (xml->sxml port)]
+         [config (get-config-elements (get-config-data (get-ecc108-content top)))]
+         [byte-list (map (lambda [x] (elementstr->elementint x)) config)])
+    (u8-list->bytevector (concatenate (map cadr byte-list)))))
+
+(define (xml-file->config-bv filename)
+  "Load the xml-file FILENAME and return a byte vector BV containing the
+contiguous bytes of the entire configuration zone"
+  (call-with-input-file filename xml-file-port->config-bv))
 
 ;;useful
 ;; From https://raw.githubusercontent.com/artyom-poptsov/guile-ssh/master/ssh/key.scm
