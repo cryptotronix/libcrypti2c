692b9585965c304b5f6ee27d552b10729427f844
Josh Datko <jbd@cryptotronix.com>
2018-03-27 09:29:11 -0600

    Changes license to LGPL, which was the intended license.

--- src/otp.c
+++ src/otp.c
@@ -1,20 +1,20 @@
 /* -*- mode: c; c-file-style: "gnu" -*-
- * Copyright (C) 2014-2015 Cryptotronix, LLC.
+ * Copyright (C) 2014-2018 Cryptotronix, LLC.
  *
- * This file is part of EClet.
+ * This file is part of libcryptoauth.
  *
- * EClet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * libcryptoauth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * EClet is distributed in the hope that it will be useful,
+ * libcryptoauth is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with EClet.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcryptoauth.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
